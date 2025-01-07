9edd4208e28f37153b507b1764c220318a910e0d
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2025-01-07 19:56:05 +0000

    Too small, making gem slightly bigger

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -33,7 +33,7 @@ export class GameScene extends Scene {
 
     private spawnGem () {
         this.gem = this.add.image(960, 540, 'gem1');
-        this.gem.setScale(0.05);
+        this.gem.setScale(0.065);
         this.gem.setInteractive();
     }
 
