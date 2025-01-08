7ffebb15c5493ef951415cbb11aa8b93bcda8d93
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2025-01-08 18:09:51 +0000

    Okay, maybe this will work now...

--- src/scenes/vectorLine.ts
+++ src/scenes/vectorLine.ts
@@ -6,10 +6,9 @@
  * Since: 2025/01/07
  */
 
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
 export class vectorLine extends Phaser.GameObjects.Graphics {
-    // Properties
     public isDrawing: boolean;
     public isLocked: boolean;
     private startPoint: Phaser.Geom.Point;
@@ -19,47 +18,43 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
         super(scene, { lineStyle: { width: 4, color: 0xffffff } });
         scene.add.existing(this);
 
-        // Initialize the properties
         this.startPoint = new Phaser.Geom.Point();
         this.isDrawing = false;
         this.isLocked = false;
     }
 
-    // The event when the pointer is down
+    // Start drawing the line
     public startDrawing(xCord: number, yCord: number) {
         this.isDrawing = true;
         this.isLocked = false;
         this.startPoint.setTo(xCord, yCord);
     }
 
-    // The event when the line is locked at a gem's position
+    // Lock the line
     public lockLine(xCord: number, yCord: number) {
         this.isLocked = true;
         this.startPoint.setTo(xCord, yCord);
     }
 
-    // The event when the pointer is moving
+    // Update the line
     public onPointerMove(pointer: Phaser.Input.Pointer) {
         this.update(pointer);
     }
 
-    // Update the line
+    // Finish drawing the line
     public update(pointer: Phaser.Input.Pointer) {
         if (this.isDrawing) {
             this.clear();
             let endX, endY;
 
-            // If the line is locked, the end point is the same as the start point
             if (this.isLocked) {
                 endX = this.startPoint.x;
                 endY = this.startPoint.y;
             } else {
-                // Else, the end point is the current pointer position
                 endX = pointer.x;
                 endY = pointer.y;
             }
 
-            // Draw the line between the start and end points
             this.lineBetween(
                 this.startPoint.x,
                 this.startPoint.y,
@@ -69,7 +64,7 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
         }
     }
 
-    // Clear the lines
+    // Clear the line
     public clearLines() {
         this.isDrawing = false;
         this.isLocked = false;
