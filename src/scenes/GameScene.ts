00c8481118c28338a15fd318a06a1104b84531f4
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2025-01-09 22:25:29 +0000

    Adding debug information to check why single gem destruction isn't working

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -39,7 +39,9 @@ export class GameScene extends Phaser.Scene {
     
         // Pointer up event for single gem destruction
         this.input.on('pointerup', () => {
+            console.log('Pointer up event triggered');
             if (this.isGemClicked && !this.isDrawingLine && this.selectedGem) {
+                console.log('Single gem destruction logic triggered');
                 this.selectedGem.destroy();
                 // Remove the gem from the array
                 for (let counter = 0; counter < this.gems.length; counter++) {
