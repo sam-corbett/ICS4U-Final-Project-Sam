25c6abb3dafc615ef75a581d5f36250a63c370a6
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2025-01-13 14:52:49 +0000

    Went back to the original, and simply added a fourth gem. I'll test what's going on.

--- src/scenes/vectorLine.ts
+++ src/scenes/vectorLine.ts
@@ -12,8 +12,8 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
     // Properties
     public isDrawing: boolean;
     public isLocked: boolean;
-    public startPoint: { xCord: number, yCord: number } = { xCord: 0, yCord: 0 };
-    public lines: { 
+    public startPoint: Phaser.Geom.Point;
+    public lockedLines: { 
         x1: number, y1: number, 
         x2: number, y2: number 
     }[] = [];
@@ -24,6 +24,7 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
         super(scene, { lineStyle: { width: 4, color: 0xffffff } });
         scene.add.existing(this);
 
+        this.startPoint = new Phaser.Geom.Point();
         this.isDrawing = false;
         this.isLocked = false;
     }
@@ -32,25 +33,25 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
     public startDrawing(xCord: number, yCord: number) {
         this.isDrawing = true;
         this.isLocked = false;
-        this.startPoint = { xCord, yCord };
+        this.startPoint.setTo(xCord, yCord);
     }
 
     // Lock the line
     public lockLine(xCord: number, yCord: number) {
         this.isLocked = true;
-        this.lines.push({
-            x1: this.startPoint.xCord,
-            y1: this.startPoint.yCord,
+        this.lockedLines.push({
+            x1: this.startPoint.x,
+            y1: this.startPoint.y,
             x2: xCord,
             y2: yCord
         });
+        this.startPoint.setTo(xCord, yCord);
     }
 
     public stopDrawing() {
         if (this.line) {
             this.line.destroy();
             this.line = null;
-            this.isLocked = false;
         }
     }
 
@@ -60,14 +61,7 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
 
     // Update the line
     public onPointerMove(pointer: Phaser.Input.Pointer) {
-        if (!this.isLocked) {
-            const endPoint = { x: pointer.x, y: pointer.y };
-            this.lines.push({
-                x1: this.startPoint.xCord,
-                y1: this.startPoint.yCord,
-                x2: endPoint.x,
-                y2: endPoint.y });
-        }
+        this.update(pointer);
     }
 
     // Finish drawing the line
@@ -78,18 +72,18 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
 
             // Draw the locked lines
             for (let counter = 0;
-                counter < this.lines.length;
+                counter < this.lockedLines.length;
                 counter++
             ) {
-                const line = this.lines[counter];
+                const line = this.lockedLines[counter];
                 this.lineBetween(line.x1, line.y1, line.x2, line.y2);
             }
 
             // Draw the current line
             let endX, endY;
             if (this.isLocked) {
-                endX = this.startPoint.xCord;
-                endY = this.startPoint.yCord;
+                endX = this.startPoint.x;
+                endY = this.startPoint.y;
             } else {
                 endX = pointer.x;
                 endY = pointer.y;
@@ -97,8 +91,8 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
 
             // Draw the line between the start and end points
             this.lineBetween(
-                this.startPoint.xCord,
-                this.startPoint.yCord,
+                this.startPoint.x,
+                this.startPoint.y,
                 endX,
                 endY
             );
@@ -109,7 +103,7 @@ export class vectorLine extends Phaser.GameObjects.Graphics {
     public clearLines() {
         this.isDrawing = false;
         this.isLocked = false;
-        this.lines = [];
+        this.lockedLines = [];
         super.clear();
     }
 }
