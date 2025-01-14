9caae9b2fc533c682ccc4c6970bd9cb1efbe3307
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2025-01-14 22:23:11 +0000

    Repositioning numGemsToSpawn

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -70,8 +70,8 @@ export class GameScene extends Phaser.Scene {
         wellDoneImage.setScale(2);
         this.time.delayedCall(2500, () => {
             wellDoneImage.destroy();
-            this.numGemsToSpawn++;
             this.showGetReady();
+            this.numGemsToSpawn++;
         });
     }
 
