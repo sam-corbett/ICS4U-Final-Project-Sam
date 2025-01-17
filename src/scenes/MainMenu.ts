593df20a6ac3ffe9f91791c266ec6bbfee666d64
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2025-01-17 18:27:51 +0000

    Rearranging the help menu icon #3 + creditScene-bg.png update

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -65,8 +65,8 @@ export class MainMenu extends Scene {
         });
 
         // Create the Advanced Help Button GameObject
-        const advancedHelpButton = this.add.image(1810, 40, 'advanced-help-button').setInteractive();
-        advancedHelpButton.setScale(0.2);
+        const advancedHelpButton = this.add.image(1805, 45, 'advanced-help-button').setInteractive();
+        advancedHelpButton.setScale(0.15);
 
         // Set the A.H.B. to direct to the Help Menu Scene
         advancedHelpButton.on('pointerdown', () => {
